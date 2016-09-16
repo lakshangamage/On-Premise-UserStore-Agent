5e312ee23a1a3515539e4d56c0c58d38fb1a09d5
Lakshan Gamage <lakshan@wso2.com>
2016-09-16 05:31:02 +0530

    Reading From userstore-config.xml is working

--- src/main/java/org/wso2/identity/cloud/config/UserStoreConfigurationXMLProcessor.java
+++ src/main/java/org/wso2/identity/cloud/config/UserStoreConfigurationXMLProcessor.java
@@ -16,40 +16,61 @@
 package org.wso2.identity.cloud.config;
 
 import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.impl.builder.StAXOMBuilder;
+import org.apache.axiom.om.OMXMLBuilderFactory;
+import org.apache.axiom.om.OMXMLParserWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.identity.cloud.constants.XMLConfigurationConstants;
+import org.wso2.identity.cloud.util.UserStoreUtils;
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamException;
 import java.io.*;
-import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 
 public class UserStoreConfigurationXMLProcessor {
+    private static Logger log = LoggerFactory.getLogger(UserStoreConfigurationXMLProcessor.class);
     protected static String USERSTORE_CONFIG_FILE = "userstore-config.xml";
     InputStream inStream = null;
-    public Map<String, String> buildRealmConfigurationFromFile(){
-        OMElement realmElement;
-        try {
-            realmElement = getRealmElement();
 
-            RealmConfiguration realmConfig = buildRealmConfiguration(realmElement);
+    public Map<String, String> buildUserStoreConfigurationFromFile(){
+        OMElement rootElement;
+        try {
+            rootElement = getRootElement();
+            Map<String,String> userstoreConfig = buildUserStoreConfiguration(rootElement);
 
             if (inStream != null) {
                 inStream.close();
             }
-            return realmConfig;
+            return userstoreConfig;
         } catch (Exception e) {
-            String message = "Error while reading realm configuration from file";
+            String message = "Error while reading userstore configuration from file";
             if (log.isDebugEnabled()) {
                 log.debug(message, e);
             }
-            throw new UserStoreException(message, e);
         }
-
+        return null;
     }
 
+    private Map<String, String> buildUserStoreConfiguration(OMElement rootElement) {
+        Map<String, String> map = new HashMap<String, String>();
+        Iterator<?> ite = rootElement.getChildrenWithName(new QName(
+                XMLConfigurationConstants.LOCAL_NAME_PROPERTY));
+        while (ite.hasNext()) {
+            OMElement propElem = (OMElement) ite.next();
+            String propName = propElem.getAttributeValue(new QName(
+                    XMLConfigurationConstants.ATTR_NAME_PROP_NAME));
+            String propValue = propElem.getText();
+            map.put(propName.trim(), propValue.trim());
+        }
+        return map;
+    }
+
+
     private OMElement getRootElement() throws XMLStreamException, IOException{
-        StAXOMBuilder builder = null;
+        OMXMLParserWrapper builder = null;
 
         File profileConfigXml = new File(UserStoreUtils.getConfigDirPath(),
                 USERSTORE_CONFIG_FILE);
@@ -57,50 +78,16 @@ public class UserStoreConfigurationXMLProcessor {
             inStream = new FileInputStream(profileConfigXml);
         }
 
-        String warningMessage = "";
         if (inStream == null) {
-            URL url;
-            if (bundleContext != null) {
-                if ((url = bundleContext.getBundle().getResource(REALM_CONFIG_FILE)) != null) {
-                    inStream = url.openStream();
-                } else {
-                    warningMessage = "Bundle context could not find resource "
-                            + REALM_CONFIG_FILE
-                            + " or user does not have sufficient permission to access the resource.";
-                }
-            } else {
-                if ((url = ClaimBuilder.class.getResource(REALM_CONFIG_FILE)) != null) {
-                    inStream = url.openStream();
-                    log.error("Using the internal realm configuration. Strictly for non-production purposes.");
-                } else {
-                    warningMessage = "ClaimBuilder could not find resource "
-                            + REALM_CONFIG_FILE
-                            + " or user does not have sufficient permission to access the resource.";
-                }
-            }
-        }
-
-        if (inStream == null) {
-            String message = "Profile configuration not found. Cause - " + warningMessage;
+            String message = "Profile configuration not found.";
             if (log.isDebugEnabled()) {
                 log.debug(message);
             }
             throw new FileNotFoundException(message);
         }
+        builder = OMXMLBuilderFactory.createOMBuilder(inStream);
+        OMElement rootElement = builder.getDocumentElement();
 
-        try {
-            inStream = CarbonUtils.replaceSystemVariablesInXml(inStream);
-        } catch (CarbonException e) {
-            throw new UserStoreException(e.getMessage(), e);
-        }
-        builder = new StAXOMBuilder(inStream);
-        OMElement documentElement = builder.getDocumentElement();
-
-        setSecretResolver(documentElement);
-
-        OMElement realmElement = documentElement.getFirstChildWithName(new QName(
-                UserCoreConstants.RealmConfig.LOCAL_NAME_REALM));
-
-        return realmElement;
+        return rootElement;
     }
 }
